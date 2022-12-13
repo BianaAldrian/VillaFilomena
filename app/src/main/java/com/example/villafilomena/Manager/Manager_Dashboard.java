320781a79ad2cc355a91459106ed88bd7601c6b9
PC-CLIENT <aldrian.scarlan@gmail.com>
2022-12-13 17:30:55 +0800

    managerDashboard update

--- app/src/main/java/com/example/villafilomena/Manager/Manager_Dashboard.java
+++ app/src/main/java/com/example/villafilomena/Manager/Manager_Dashboard.java
@@ -3,16 +3,27 @@ package com.example.villafilomena.Manager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.villafilomena.R;
 
 public class Manager_Dashboard extends AppCompatActivity {
+
+    ImageView addVideo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.manager_dashboard);
 
+        addVideo = findViewById(R.id.add_homeVideo);
 
+        addVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
