3f93a4e443c4cb3ebc0df554add4706a3fc0a722
Meng <curamengsheerymae@gmail.com>
2023-01-04 23:52:37 +0800

    update 6.11

---
+++ app/src/main/java/com/example/villafilomena/TermsandCondition.java
@@ -0,0 +1,14 @@
+package com.example.villafilomena;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TermsandCondition extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.termsand_condition);
+    }
+}
