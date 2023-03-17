f810d3e8f277ec47802d4acc0b9a5957b1a38516
BianaAldrian <aldrian.primary@gmail.com>
2023-03-17 16:20:11 +0800

    test

--- app/src/main/java/com/example/villafilomena/TermsandCondition.java
+++ app/src/main/java/com/example/villafilomena/TermsandCondition.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class TermsandCondition extends AppCompatActivity {
+
+    //github token
+    //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
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
